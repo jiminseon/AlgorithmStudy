7a08f27499a380a5e7a036eea5db2084a8acad40
Ji minseon <88239886+jiminseon@users.noreply.github.com>
2024-07-30 14:55:05 +0900

    [Silver I] Title: 회의실 배정, Time: 1208 ms, Memory: 174072 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] a = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            a[i][0] = sc.nextInt();
+            a[i][1] = sc.nextInt();
+        }
+        Arrays.sort(a, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] s, int[] e) {
+                if (s[1] == e[1]) {
+                    return s[0] - e[0];
+                }
+                return s[1] - e[1];
+            }
+        });
+        int count = 0;
+        int end = -1;
+        for (int i = 0; i < n; i++) {
+            if (a[i][0] >= end) {
+                end = a[i][1];
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
