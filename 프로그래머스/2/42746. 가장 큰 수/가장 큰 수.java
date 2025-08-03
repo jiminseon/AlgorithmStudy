c6bfbe9e504aa2a3d7825bb7e8c278ea6121b1d2
jiminseon <88239886+jiminseon@users.noreply.github.com>
2025-08-03 13:30:30 +0900

    [level 2] Title: 가장 큰 수, Time: 2840.77 ms, Memory: 394 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+
+  
+        String[] arr = new String[numbers.length];
+
+        for(int i=0; i<numbers.length; i++){
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+
+        if(arr[0].equals("0")){
+            answer = "0";
+        }
+        else{
+            for (int i=0; i<arr.length; i++) {
+                answer += arr[i];
+            }
+        }
+
+        return answer;
+    }
+}
