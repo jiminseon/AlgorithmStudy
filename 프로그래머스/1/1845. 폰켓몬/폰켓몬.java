bec31f870267fb300fc0a25bf40e68131a996617
jiminseon <88239886+jiminseon@users.noreply.github.com>
2025-08-08 16:28:23 +0900

    [level 1] Title: 폰켓몬, Time: 8.51 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            map.putIfAbsent(nums[i], 0);
+            map.put(nums[i], map.get(nums[i] + 1));
+        }
+        
+        if (map.size() > nums.length / 2) {
+            answer = nums.length / 2;
+        } else {
+            answer = map.size();   
+        }
+        
+        return answer;
+    }
+}
