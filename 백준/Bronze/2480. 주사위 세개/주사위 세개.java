bbbf8854bf8f6b2b471d6c3da147223b8e478777
jiminseon <88239886+jiminseon@users.noreply.github.com>
2025-12-29 19:45:48 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)   {
+        Scanner sc = new Scanner(System.in);
+
+        int a =  sc.nextInt();
+        int b =  sc.nextInt();
+        int c =  sc.nextInt();
+
+        int result = 0;
+        if (a == b && a == c) {
+            result = 10000 + a * 1000;
+        } else if (a == b || (a == c)) {
+            result = 1000 + a * 100;
+        } else if (b == c) {
+            result = 1000 + b * 100;
+        }
+        else {
+            int max = Math.max(a, Math.max(b, c));
+            result = max * 100;
+        }
+
+        System.out.print(result);
+    }
+}
