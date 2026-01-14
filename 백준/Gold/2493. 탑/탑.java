d6d0c8f07d0bac3c592da57eddb758e0c4f8274a
jiminseon <88239886+jiminseon@users.noreply.github.com>
2026-01-14 22:09:14 +0900

    [Gold V] Title: 탑, Time: 2756 ms, Memory: 127684 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,23 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Deque<int[]> stack = new ArrayDeque<>();
 
-        Stack<int[]> stack = new Stack<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++) {
             int top = Integer.parseInt(st.nextToken());
             while(!stack.isEmpty()) {
-                if(stack.peek()[1] >= top) { //stack.peek()[1] 탑의 높이
-                    System.out.print(stack.peek()[0] + " "); //(stack.peek()[0] i(탑의 위치)값
+                if(stack.peek()[1] >= top) {
+                    System.out.print(stack.peek()[0] + " ");
                     break;
                 }
                 stack.pop();
