b4e0fa5d2499bf50d3cfbd7f33d387e56ad88f76
jiminseon <88239886+jiminseon@users.noreply.github.com>
2026-01-30 22:02:18 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] arr = new boolean[31];
+        for (int i = 0; i < 28; i++) {
+            arr[Integer.parseInt(br.readLine())] = true;
+        }
+
+        for (int i = 1; i <= 30; i++) {
+            if (arr[i]) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+
+
+}
