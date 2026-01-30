d509a0140b62ffff285433821c3318fbd1d5628d
jiminseon <88239886+jiminseon@users.noreply.github.com>
2026-01-30 16:13:18 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[100][100];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    arr[j][k] = true;
+                }
+            }
+        }
+
+        int area = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j]) area++;
+            }
+        }
+
+        System.out.print(area);
+    }
+
+
+}
